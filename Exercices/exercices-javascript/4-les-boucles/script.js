1b6fec6f35f5ba555b34b1bcff792a8cbecb2902
Léo <66998902+Baheza-Lionel@users.noreply.github.com>
2020-07-15 11:44:51 +0200

    update

--- Exercices/exercices-javascript/4-les-boucles/script.js
+++ Exercices/exercices-javascript/4-les-boucles/script.js
@@ -9,11 +9,13 @@ console.log(fizzBuzzArray);
 // Exercice 2: Toujours à l'aide d'une boucle, passe sur chaque élément du tableau que tu viens de créer (fizzBuzzArray), si l'élément est divisible par trois affiche "fizz" dans la console, si l'élément est divisible par cinq affiche "buzz", si il est divisible par trois et par cinq affiche "fizzBuzz"
 fizzBuzzArray.forEach(element => {
   if (element % 3 === 0 && element % 5 === 0) {
-    console.log('fizzBuzz');
+    console.log('fizzBuzzArray');
   } else if (element % 3 === 0) {
     console.log('fizz');
   } else if (element % 5 === 0) {
     console.log('buzz');
   }
+
 });
+
 // Exercice 3: A l'aide de l'objet Date (voir: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date) affiche tous les vendredi 13 de l'année 2020
