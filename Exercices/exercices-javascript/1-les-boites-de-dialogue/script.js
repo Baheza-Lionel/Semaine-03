85e7b5a54cc4b232b40e278a652ecb46c1cb3813
Léo <66998902+Baheza-Lionel@users.noreply.github.com>
2020-07-15 11:55:56 +0200

    update

--- Exercices/exercices-javascript/1-les-boites-de-dialogue/script.js
+++ Exercices/exercices-javascript/1-les-boites-de-dialogue/script.js
@@ -1,18 +1,23 @@
 // Exercice 1: affiche Hello World dans une boite de dialogue alert();
-const greeting = 'Hello world';
-console.log(greeting);
-window.alert('greeting');
-// Exercice 2: Place ton code précédent en commentaire, dans un prompt, demande son prénom à l'utilisateur, ensuite, à l'aide d'un alert() affiche "Salut" + le prénom de l'utilisateur
-let Greeting = prompt('Hello world');
+const Greeting = 'Hello World';
 console.log(Greeting);
-let whatsYourName = prompt("comment t'appelles tu?", 'ex: Lionel');
+window.alert('Greeting');
+// Exercice 2: Place ton code précédent en commentaire, dans un prompt, demande son prénom à l'utilisateur, ensuite, à l'aide d'un alert() affiche "Salut" + le prénom de l'utilisateur
+let greeting = prompt('Hello World');
+console.log(Greeting);
+let whatsYourName = prompt("comment t'appelles-tu?", 'ex: Lionel');
 console.log(whatsYourName);
-let greet = 'salut';
-console.log(greet);
-window.alert('salut' + '' + 'Lionel');
+let greet = 'Salut';
+let name = "Lionel"
+console.log(greet, name);
+window.alert("Salut" + " " + "Lionel");
 // Exercice 3: Place ton code précédent en commentaire. Dans un prompt, demande son âge à l'utilisateur, ensuite, à l'aide d'un .confirm() vérifie si l'âge de l'utilisateur est bien supérieur à 18 ans, si oui, demande à l'utilisateur de confirmer son âge et affiche "Bienvenue" dans une alert()
-let howOldareYou = prompt(' quel âge avez-vous?');
+
+let howOldareYou = prompt(' Quel âge avez-vous?');
 console.log(howOldareYou);
-if (window.confirm('element => element < 18')) {
-  window.open('contact.html', 'Vous êtes redirigé vers la page de contact');
-}
+window.alert("Bienvenu");
+if (window.confirm("Êtes-vous inférieur de 18 ans?"))
+  window.alert("Confirmer votre âge")
+else (window.confirm("Êtes-vous supérieur de 18 ans"))
+window.alert("Bienvenu");
+
